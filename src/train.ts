cad394e6cbd746ad0e80e96023de962334ff4f58
Azizbek Siddikov <azbeksid@gmail.com>
2024-12-09 09:53:47 +0900

    feat: create a train area

---
+++ src/train.ts
@@ -0,0 +1,20 @@
+// G-TASK:
+
+// Shunday function tuzingki unga integerlardan iborat array pass bolsin va
+// function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+// MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+
+// function getHighestIndex(arr: number[]): number {
+//   let max_num: number = Number.MIN_VALUE,
+//     max_idx: number = 0;
+//   for (let index = 0; index < arr.length; index++) {
+//     if (arr[index] > max_num) {
+//       max_num = arr[index];
+//       max_idx = index;
+//     }
+//   }
+//   return max_idx;
+// }
+
+// console.log(getHighestIndex([5, 21, 12, 21, 8])); // 1
+// console.log(getHighestIndex([5, 21, 12, 21, 50, 8])); // 4
