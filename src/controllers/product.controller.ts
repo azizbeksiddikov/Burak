cc692438dee442c5ab12089725e5196183ef5482
azizbeksiddikov <azbeksid@gmail.com>
2024-12-23 22:34:53 +0900

    feat: create product controller and product routing apis

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,49 @@
+import { T } from "../libs/types/common";
+import { Request, Response } from "express";
+import ProductService from "../models/Product.service";
+import { Member, MemberInput, LoginInput } from "../libs/types/member";
+import Errors, { Message } from "../libs/Errors";
+
+const productService = new ProductService();
+const productController: T = {};
+
+productController.getAllProducts = async (req: Request, res: Response) => {
+  try {
+    console.log("getAllProducts");
+    res.render("products");
+  } catch (err) {
+    console.log("Error, getAllProducts:", err);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(
+      `<script> alert("${message}"); window.location.replace("admin/signup")</script>`
+    );
+  }
+};
+
+productController.createNewProduct = async (req: Request, res: Response) => {
+  try {
+    console.log("createNewProduct");
+  } catch (err) {
+    console.log("Error, createNewProduct:", err);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(
+      `<script> alert("${message}"); window.location.replace("admin/signup")</script>`
+    );
+  }
+};
+
+productController.updateChosenProduct = async (req: Request, res: Response) => {
+  try {
+    console.log("updateChosenProduct");
+  } catch (err) {
+    console.log("Error, updateChosenProduct:", err);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(
+      `<script> alert("${message}"); window.location.replace("admin/signup")</script>`
+    );
+  }
+};
+export default productController;
