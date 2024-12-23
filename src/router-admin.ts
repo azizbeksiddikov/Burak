3f5352f9c94824c1f1e9886378643e7916d2c8cc
azizbeksiddikov <azbeksid@gmail.com>
2024-12-24 00:03:48 +0900

    fix: upload multiple images for productImages

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -28,7 +28,7 @@ routerAdmin.get(
 routerAdmin.post(
   "/product/create",
   restaurantController.verifyRestaurant,
-  makeUploader("products").single("productImage"),
+  makeUploader("products").array("productImages", 5),
   productController.createNewProduct
 );
 routerAdmin.post(
