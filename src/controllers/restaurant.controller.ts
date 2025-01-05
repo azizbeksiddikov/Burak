bfde0a5b0b22f72beefe1dcf0bdda7593d3b0913
azizbeksiddikov <azbeksid@gmail.com>
2025-01-05 10:08:48 +0900

    feat: develop signup fe validation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -55,7 +55,6 @@ restaurantController.processSignup = async (
 
     const newMember: MemberInput = req.body;
     newMember.memberImage = file?.path.replace(/\\/g, "");
-
     newMember.memberType = MemberType.RESTAURANT;
 
     const result = await memberService.processSignup(newMember);
