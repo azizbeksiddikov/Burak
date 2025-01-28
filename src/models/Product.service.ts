6d3d82e4948266ea6a997acd3460e4373e9d922d
azizbeksiddikov <azbeksid@gmail.com>
2025-01-28 19:14:58 +0900

    feat: develop getProducts rest api

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -3,20 +3,49 @@ import {
   ProductInput,
   Product,
   ProductUpdateInput,
+  ProductInquiry,
 } from "../libs/types/product";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { shapeIntoMongooseObjectId } from "../libs/config";
+import { T } from "../libs/types/common";
+import { ProductStatus } from "../libs/enums/product.enum";
 
 class ProductService {
-  private readonly ProductModel;
+  private readonly productModel;
   constructor() {
-    this.ProductModel = ProductModel;
+    this.productModel = ProductModel;
   }
   // SPA
+  public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
+    const match: T = { productStatus: ProductStatus.PROCESS };
+
+    if (inquiry.productCollection)
+      match.productCollection = inquiry.productCollection;
+    if (inquiry.search)
+      match.productName = { $regex: new RegExp(inquiry.search, "i") };
+
+    const sort: T =
+      inquiry.order === "productPrice"
+        ? { [inquiry.order]: 1 }
+        : { [inquiry.order]: -1 };
+
+    const result = await this.productModel
+      .aggregate([
+        { $match: match },
+        { $sort: sort },
+        { $skip: (inquiry.page * 1 - 1) * inquiry.limit },
+        { $limit: inquiry.limit * 1 },
+      ])
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result as unknown as Product[];
+  }
 
   // SSR
   public async getAllProducts(): Promise<Product> {
-    const result = await this.ProductModel.find().exec();
+    const result = await this.productModel.find().exec();
     // console.log(result);
 
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
@@ -27,7 +56,7 @@ class ProductService {
   public async createNewProduct(input: ProductInput): Promise<Product[]> {
     try {
       console.log("entered createNewProduct");
-      const result = await this.ProductModel.create(input);
+      const result = await this.productModel.create(input);
       console.log("leaving createNewProduct");
 
       return result as unknown as Product[];
@@ -43,11 +72,9 @@ class ProductService {
   ): Promise<Product> {
     // string => ObjectId
     id = shapeIntoMongooseObjectId(id);
-    const result = await this.ProductModel.findByIdAndUpdate(
-      { _id: id },
-      input,
-      { new: true }
-    ).exec();
+    const result = await this.productModel
+      .findByIdAndUpdate({ _id: id }, input, { new: true })
+      .exec();
 
     if (!result) {
       throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
